c928c5a1928bf7a4a549d4b5fb2d9e42e37d196c
Shazzadul Islam Fahim <shazzadulislamfahim@gmail.com>
2024-03-19 11:57:27 +0600

    Login Logout

---
+++ src/test/java/LoginTestRunner.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTestRunner extends Setup {
+    @Test
+    public void login(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.doLogin("Admin","admin123");
+        WebElement imgProfile = driver.findElement(By.className("oxd-userdropdown-img"));
+        Assert.assertTrue(imgProfile.isDisplayed());
+    }
+}
